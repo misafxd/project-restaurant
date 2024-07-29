899b0d60ca2562030cc85fee3d3bcb43267857d4
misafxd <pisichu.mafs@gmail.com>
2024-07-29 00:24:21 -0600

    Add listeners to each button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import './style.css';
 import { homePage } from './home.js';
 import { menu } from './menu.js';
+import { about } from './about.js';
+
+function wipeOut() {
+    const content = document.querySelector('.content');
+    content.innerHTML = '';
+}
+
+function page() {
+    const homeButton = document.getElementById('home');
+    const menuButton = document.getElementById('menu');
+    const aboutButton = document.getElementById('about');
 
 
-menu();
-// homePage()
+    homeButton.addEventListener('click', () =>{
+        wipeOut();
+        homePage();
+    })
+
+    menuButton.addEventListener('click', () => {
+        wipeOut();
+        menu();
+    })
+
+    aboutButton.addEventListener('click', () => {
+        wipeOut();
+        about();
+    })
+    homePage();
+
+}
+
+page();
